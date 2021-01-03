73cc794b8b6bef3615e4f86f4201bb838c0c70bd
oerms <oerms@gmx.net>
2021-01-03 18:11:46 +0100

    day11_part2

--- day11/advent11.py
+++ day11/advent11.py
@@ -13,75 +13,96 @@ def initializeSeats(filename):
             seats.append([(0 if char=='L' else -1) for char in line])
     return np.array(seats)
 
-def evolveSeats(seats,tempSeats):
+def evolveSeats(seats,tempSeats,rules="old"):
     """evolve reactor grid pocket by one cycle"""
     for (i,j) in [(i,j) for i in range(len(seats)) for j in range(len(seats[0]))]:
         if not seats[i,j] == -1:
-            numOccupiedNear = countNeighbors(seats,i,j)
-            #if i<3 and j<2:
-                #print(i,j)
-                #print(numOccupiedNear)
-            if seats[i,j] == 0 and numOccupiedNear == 0:
-                tempSeats[i,j] = 1
-            elif seats[i,j] == 1 and numOccupiedNear >= 4:
-                tempSeats[i,j] = 0
-            else:
-                tempSeats[i,j] = seats[i,j]
+            if rules == "old":
+                numOccupiedNear = countNeighbors(seats,i,j)
+                if seats[i,j] == 0 and numOccupiedNear == 0:
+                    tempSeats[i,j] = 1
+                elif seats[i,j] == 1 and numOccupiedNear >= 4:
+                    tempSeats[i,j] = 0
+                else:
+                    tempSeats[i,j] = seats[i,j]
+            elif rules == "new":
+                numOccupiedNear = countNeighbors(seats,i,j,countdepth=max(seats.shape))
+                if seats[i,j] == 0 and numOccupiedNear == 0:
+                    tempSeats[i,j] = 1
+                elif seats[i,j] == 1 and numOccupiedNear >= 5:
+                    tempSeats[i,j] = 0
+                else:
+                    tempSeats[i,j] = seats[i,j]
     return tempSeats, seats
 
 def countOccupiedSeats(seats):
     """count occupied seats of given array of seats"""
     return (seats == 1).sum()
 
-def countNeighbors(seats,xcoord,ycoord):
+def countNeighbors(seats,xcoord,ycoord,countdepth=1):
     """count neighbors of coords in seat array"""
     count = 0
-    for (x,y) in [(xcoord+i,ycoord+j) for i in range(-1,2) for j in range(-1,2)]:
-        if x >= 0 and x < len(seats) and \
-           y >= 0 and y < len(seats) and \
-           seats[x,y] == 1:
-            count += 1
+    # loop through directions
+    for (i,j) in [(i,j) for i in range(-1,2) for j in range(-1,2)]:
+        # move as deep as requested
+        for depth in range(1,countdepth+1):
+            x = xcoord + (i*depth)
+            y = ycoord + (j*depth)
+            # if within the seating area: do the counting
+            if x >= 0 and x < len(seats) and y >= 0 and y < len(seats[0]):
+                # if seat is taken, counter++ and go to next direction
+                if seats[x,y] == 1:
+                    count += 1
+                    break
+                # if seat is empty, no counter and go to next direction                 
+                elif seats[x,y] == 0:
+                    break
+                # if there is no seat (== -1), go deeper
+                else:
+                    continue
+            # if out of seating, go to next direction
+            else:
+                break
     if seats[xcoord,ycoord]==1:
         count -= 1
     return count
 
 if __name__ == "__main__":
-    # change order of following for less steps or testcase
+    # change order of following for testcase
     filename = "test11"
     filename = "input11"
 
     print('part 1')
     seats = initializeSeats(filename)
-    #print('initial state')
-
-    cycle = 0
     tempSeats = seats.copy()
     occupiedSeats = countOccupiedSeats(seats)
-    #print(seats)
-    #print('occupied seats after cycle',cycle,':',occupiedSeats)
+    cycle = 0
     oldOccupied = 0
     while True:
         oldOccupied = occupiedSeats
         seats, tempSeats = evolveSeats(seats,tempSeats)
         cycle += 1
-        #print(seats)
         occupiedSeats = countOccupiedSeats(seats)
         #print('occupied seats after cycle',cycle,':',occupiedSeats)
         #print('occupied seats after last cycle',cycle-1,':',oldOccupied)
         if oldOccupied == occupiedSeats:
-            print('no change with last cycle! occupied seats:',occupiedSeats)
+            print('no change with last cycle! occupied seats:',occupiedSeats, 'after',cycle-1,'cycles')
             break
-        #if cycle == 3 :
-            #break
 
-    exit()
-    print('part 2: four dimensions')
-    grid4 = initializeReactor4(filename,numCycles)
-    #print('initial state')
-    #printGrid4(grid4,int(len(grid4)/2),int(len(grid4)/2))
-
-    for cycle in range(numCycles):
-        grid4 = evolveGrid4(grid4)
-        print('sum after cycle',cycle+1,':',grid4.sum())
-        #printGrid4(grid4,int(len(grid4)/2),int(len(grid4)/2))
-
+    print('part 2: count along directions')
+    seats = initializeSeats(filename)
+    tempSeats = seats.copy()
+    occupiedSeats = countOccupiedSeats(seats)
+    cycle = 0
+    oldOccupied = 0
+    while True:
+        oldOccupied = occupiedSeats
+        seats, tempSeats = evolveSeats(seats,tempSeats,rules="new")
+        cycle += 1
+        occupiedSeats = countOccupiedSeats(seats)
+        #print('occupied seats after cycle',cycle,':',occupiedSeats)
+        #print('occupied seats after last cycle',cycle-1,':',oldOccupied)
+        if oldOccupied == occupiedSeats:
+            print('no change with last cycle! occupied seats:',occupiedSeats, 'after',cycle-1,'cycles')
+            break
+ 
