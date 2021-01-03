51708896cbfcf5eef97ee38e1d169ab14d3387ed
oerms <oerms@gmx.net>
2021-01-03 20:55:58 +0100

    day12_part2

--- day12/advent12.py
+++ day12/advent12.py
@@ -26,6 +26,25 @@ def navigateStep(coord, direction, step):
     else:
         raise
 
+def navigateByWaypoint(coord, waypoint, step):
+    """update coordinate and direction, as per the step instruction"""
+    if step[0]=='N':
+        return coord, waypoint + 1j*int(step[1:])
+    elif step[0]=='S':
+        return coord, waypoint - 1j*int(step[1:])
+    elif step[0]=='E':
+        return coord, waypoint + int(step[1:])
+    elif step[0]=='W':
+        return coord, waypoint - int(step[1:])
+    elif step[0]=='L':
+        return coord, waypoint*cmath.rect(1, int(step[1:])*cmath.pi/180)
+    elif step[0]=='R':
+        return coord, waypoint*cmath.rect(1,-int(step[1:])*cmath.pi/180)
+    elif step[0]=='F':
+        return coord + waypoint*int(step[1:]), waypoint
+    else:
+        raise
+
 if __name__ == "__main__":
     # change order of following for testcase
     filename = "test12"
@@ -35,17 +54,25 @@ if __name__ == "__main__":
     # using complex numbers...
     coord = complex(0)
     direction = complex(1) # 1 == east, 1j == north, -1 == west, -1j == south
-    print('coordinates:', coord)
-    print('direction:', direction)
 
     with open(filename) as fileH:
         for line in fileH:
-            print(line.strip())
             coord, direction = navigateStep(coord, direction, line.strip())
-            print('coordinates:', coord)
-            print('direction:', direction)
 
     print('final coordinates:',coord)
     print('final direction:', direction)
-    print('final distance:', abs(coord.real) + abs(coord.imag))
+    print('final distance:', round(abs(coord.real) + abs(coord.imag)))
+
+    print('part 2: setting the waypoints...')
+    # using complex numbers...
+    coord = complex(0)
+    waypoint = complex(10 + 1j) # 1 == east, 1j == north, -1 == west, -1j == south
+
+    with open(filename) as fileH:
+        for line in fileH:
+            coord, waypoint = navigateByWaypoint(coord, waypoint, line.strip())
+
+    print('final coordinates:',coord)
+    print('final waypoint:', waypoint)
+    print('final distance:', round(abs(coord.real) + abs(coord.imag)))
 
