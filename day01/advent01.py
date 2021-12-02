c185aa964fe8b6d9130edbd685cf1a2fce440b98
oerms <oerms@gmx.net>
2021-12-02 21:46:36 +0100

    day01

--- day01/advent01.py
+++ day01/advent01.py
@@ -3,60 +3,25 @@
 # advent of code 2021, day 01
 # oerms
 
-import copy
+import numpy as np
 
-class InfiniteLoop(Exception):
-    """Exception class for infinite loops"""
-    def __init__(self, position, message="Infinite loop detected"):
-        self.position= position
-        self.message = message
-
-    def __str__(self):
-        return self.message
-
-def loadProgram(filename):
-    """load rules from filename"""
-    with open(filename) as fileH:
-        for line in fileH:
-            coord, direction = navigateStep(coord, direction, line.strip())
-
-    with open(filename) as fileH:
-        program = [ [line.split()[0], int(line.split()[1])] for line in fileH ]
-    return program
+def readfile(filename):
+    """read file to get readings"""
+    return np.loadtxt(filename,dtype=int)
 
 if __name__ == "__main__":
     # change order of following for testcase
-    filename = "test8"
-    filename = "input8"
-    program = loadProgram(filename)
+    filename = "test01"
+    filename = "input01"
 
-    print('part 1')
-    try:
-        accumulator = runProgram(program)
-        print(accumulator)
-    except InfiniteLoop as err:
-        print(err)
-        print('recurring line:', err.position+1, '\naccumulator:', err.accumulator)
+    readings = readfile(filename)
+    print(readings)
 
+    print('part 1: sonar sweep')
+    num_increases = sum([1 for i in range(len(readings)-1) if readings[i] < readings[i+1]])
+    print(num_increases)
 
-    filename = "test12"
-    filename = "input12"
-
-    print('part 1: moving along...')
-    # using complex numbers...
-    coord = complex(0)
-    direction = complex(1) # 1 == east, 1j == north, -1 == west, -1j == south
-
-    with open(filename) as fileH:
-        for line in fileH:
-            coord, direction = navigateStep(coord, direction, line.strip())
-
-    print('final coordinates:',coord)
-    print('final direction:', direction)
-    print('final distance:', round(abs(coord.real) + abs(coord.imag)))
-
-    print('part 2: setting the waypoints...')
-    # using complex numbers...
-    coord = complex(0)
-    waypoint = complex(10 + 1j) # 1 == east, 1j == north, -1 == west, -1j == south
+    print('part 2: sonar sweep - windowed')
+    num_increases_window = sum([1 for i in range(len(readings)-3) if readings[i]+readings[i+1]+readings[i+2] < readings[i+1]+readings[i+2]+readings[i+3]])
+    print(num_increases_window)
 
